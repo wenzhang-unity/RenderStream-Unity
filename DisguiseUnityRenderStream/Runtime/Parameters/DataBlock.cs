ac43549c3c9b9676538ea5dc3077146e5003fc56
Mark Beiline <mark.beiline@unity3d.com>
2023-04-06 16:42:37 -0400

    Added cached update mechanism

--- DisguiseUnityRenderStream/Runtime/Parameters/DataBlock.cs
+++ DisguiseUnityRenderStream/Runtime/Parameters/DataBlock.cs
@@ -100,16 +100,14 @@ namespace Disguise.RenderStream.Parameters
 
                 if (consumeParameterSpan)
                 {
-                    var cpuData = new SceneStoredCPUData
-                    {
-                        Numeric = Numeric.GetMemory(numericParameterIndex, numericParametersFound),
-                        Text = Text.GetMemory(textParameterIndex, textParametersFound)
-                    };
+                    var cpuData = new SceneStoredCPUData();
+                    cpuData.SignalChange();
+                    cpuData.Numeric = Numeric.GetMemory(numericParameterIndex, numericParametersFound, cpuData);
+                    cpuData.Text = Text.GetMemory(textParameterIndex, textParametersFound, cpuData);
 
-                    var gpuData = new SceneStoredGPUData
-                    {
-                        Textures = Textures.GetMemory(textureParameterIndex, textureParametersFound)
-                    };
+                    var gpuData = new SceneStoredGPUData();
+                    gpuData.SignalChange();
+                    gpuData.Textures = Textures.GetMemory(textureParameterIndex, textureParametersFound, gpuData);
 
                     CPUData.Add(cpuData);
                     GPUData.Add(gpuData);
@@ -154,16 +152,20 @@ namespace Disguise.RenderStream.Parameters
             ApplyData(GPUData, (parameter, data) => parameter.ApplyData(data.ToReadOnly()));
         }
         
-        void ApplyData<TData>(IList<TData> data, Action<IRemoteParameterWrapper, TData> applyData)
+        void ApplyData<TData>(IList<TData> data, Action<IRemoteParameterWrapper, TData> applyData) where TData : ChangeTracker
         {
             if (RemoteParameters.Count != data.Count)
                 throw new InvalidOperationException("Data and parameter lists size mismatch");
             
             for (var i = 0; i < RemoteParameters.Count; i++)
             {
-                var remoteParameter = RemoteParameters[i];
                 var element = data[i];
-                applyData.Invoke(remoteParameter, element);
+                if (element.HasChange)
+                {
+                    var remoteParameter = RemoteParameters[i];
+                    applyData.Invoke(remoteParameter, element);
+                    element.ConsumeChange();
+                }
             }
         }
 
@@ -189,11 +191,29 @@ namespace Disguise.RenderStream.Parameters
             }
         }
     }
+
+    abstract class ChangeTracker
+    {
+        bool m_ChangeFlag;
+
+        public bool HasChange => m_ChangeFlag;
+        
+        public void SignalChange()
+        {
+            m_ChangeFlag = true;
+        }
+
+        public void ConsumeChange()
+        {
+            m_ChangeFlag = false;
+        }
+    }
     
     /// <summary>
-    /// An internal mutable representation of <see cref="SceneCPUData"/>.
+    /// An internal heap-friendly representation of <see cref="SceneCPUData"/>.
+    /// Contains a span of relevant <see cref="DataBlock{T}"/> for an <see cref="IRemoteParameterWrapper"/>.
     /// </summary>
-    struct SceneStoredCPUData
+    class SceneStoredCPUData : ChangeTracker
     {
         public ReadOnlyMemory<float> Numeric;
         public ReadOnlyMemory<string> Text;
@@ -205,9 +225,10 @@ namespace Disguise.RenderStream.Parameters
     }
     
     /// <summary>
-    /// An internal mutable representation of <see cref="SceneGPUData"/>.
+    /// An internal heap-friendly representation of <see cref="SceneGPUData"/>.
+    /// Contains a span of relevant <see cref="DataBlock{T}"/> for an <see cref="IRemoteParameterWrapper"/>.
     /// </summary>
-    struct SceneStoredGPUData
+    class SceneStoredGPUData : ChangeTracker
     {
         public ReadOnlyMemory<Texture> Textures;
         
@@ -250,20 +271,40 @@ namespace Disguise.RenderStream.Parameters
     /// </summary>
     class DataBlock<T>
     {
+        /// <summary>
+        /// Holds a flat list of data received from Disguise.
+        /// </summary>
         readonly T[] m_Data;
+        
+        /// <summary>
+        /// The <see cref="ChangeTracker"/>s associated to each element in <see cref="m_Data"/>.
+        /// </summary>
+        readonly ChangeTracker[] m_ChangeTrackers;
         
         public DataBlock(int length)
         {
             m_Data = new T[length];
+            m_ChangeTrackers = new ChangeTracker[length];
         }
 
-        public Memory<T> GetMemory(int start, int length)
+        public Memory<T> GetMemory(int start, int length, ChangeTracker changeTracker)
         {
+            for (var i = start; i < start + length; i++)
+            {
+                if (m_ChangeTrackers[i] != null)
+                    throw new InvalidOperationException($"Overlapping remote parameter mappings at element {i}");
+
+                m_ChangeTrackers[i] = changeTracker;
+            }
+            
             return new Memory<T>(m_Data, start, length);
         }
 
         public void SetValue(int index, T value)
         {
+            if (!m_Data[index].Equals(value))
+                SignalChange(index);
+            
             m_Data[index] = value;
         }
         
@@ -275,12 +316,27 @@ namespace Disguise.RenderStream.Parameters
             // We can't accept a NativeArray directly because it would force T to be a value type.
             
             if (m_Data.Length != dataLength)
-                throw new InvalidOperationException($"The provided {nameof(dataLength)} and the internal capacity are different.");
+                throw new InvalidOperationException($"The provided {nameof(dataLength)} ({dataLength}) and the internal capacity ({m_Data.Length}) do not match.");
 
             for (var i = 0; data.MoveNext() && i < m_Data.Length; i++)
             {
-                m_Data[i] = data.Current;
+                var newData = data.Current;
+                
+                if (!m_Data[i].Equals(newData))
+                    SignalChange(i);
+                
+                m_Data[i] = newData;
             }
+        }
+
+        void SignalChange(int index)
+        {
+            var changeTracker = m_ChangeTrackers[index];
+            
+            if (changeTracker == null)
+                throw new InvalidOperationException($"No change tracker registered for data element {index}");
+            
+            changeTracker.SignalChange();
         }
     }
 }
